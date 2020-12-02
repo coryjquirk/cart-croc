ac58260f349a579f1bfb805ba0017c92e603430f
Josh8903 <thorngren87@gmail.com>
2020-12-02 14:15:33 -0600

    homepage links

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -4,7 +4,12 @@ import { Col, Row, Container } from "../components/Grid";
 const Home = () => {
   return (
     <Container fluid>
-        {/* module in here */}
+      <a href="/newsale">New Sale</a>
+      <br />
+      <a href="/receipts">Receipts</a>
+      <br />
+      <a href="/inventory">Inventory</a>
+      <br />
     </Container>
   );
 };
