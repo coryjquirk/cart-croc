96678051f2cf9ffe9085a2a006c8201ac5cb42f9
Cory Quirk <coryjquirk@gmail.com>
2020-12-02 15:23:58 -0600

    less crap nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,10 +7,36 @@ function Nav() {
   const [store] = useStoreContext();
 
   return (
-        <div id="crocNav">
-          <h1>Cart Croc</h1>
-          <img src={Logo} alt="Logo" id="croc"/>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light" id="crocNav">
+        <img src={Logo} alt="Logo" id="croc"/>
+        <a class="navbar-brand" href="/">Cart Croc</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="completesale">complete sale</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="home">home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="inventory">inventory</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="newsale">new sale</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="printreceipt">print receipt</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="receipts">receipts</a>
+            </li>
+            </ul>
         </div>
+        {store.loading ? <a className="navbar-brand ml-auto">loading...</a> : <></>}
+    </nav>
     );
 }
 
