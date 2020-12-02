5d56fba09424f6473a5a0169de6e34b9bd5f595f
Cory Quirk <coryjquirk@gmail.com>
2020-12-02 14:32:23 -0600

    logo

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Col, Row, Container } from "../components/Grid";
 import Signup from "../components/SignUp";
+import Logo from '../components/Logo/logo.png';
 
 const Index = () => {
   return (
     <Container fluid>
+        <img src={Logo} alt="Logo" />
         <Signup />
+  
+        
     </Container>
   );
 };
