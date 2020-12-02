d83f39bd1431366837d7eb72396336a31262a56b
Cory Quirk <coryjquirk@gmail.com>
2020-12-02 14:44:05 -0600

    logo

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -6,6 +6,7 @@ import Logo from '../components/Logo/logo.png';
 const Index = () => {
   return (
     <Container fluid>
+      <img src={Logo} alt="Logo" />
       <Signup />
       <br />
       <a href="/home">Login bypass</a>
