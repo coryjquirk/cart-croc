5e590337ec849a9ea35b4d89ada11689b95374e7
Cory Quirk <coryjquirk@gmail.com>
2020-12-03 11:02:18 -0600

    working on modal/carousel for inventory

--- client/src/components/Product/index.js
+++ client/src/components/Product/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+import ReactImageMagnify from 'react-image-magnify';
+//usage guide: https://www.npmjs.com/package/react-image-magnify
+var Modal = require('react-bootstrap-modal')
 import { useStoreContext } from "../../utils/GlobalState";
 import "./style.css";
-import Tester from './product.jpg';
+import Tester from './product1.jpg';
 import Texture from '../Images/45-degree-fabric-light.png';
 // https://www.transparenttextures.com/
 
@@ -11,11 +14,22 @@ var cardStyle = {
 
 function Product() {
   const [store] = useStoreContext();
-
   return (
         <div id="productCard" style={cardStyle}>
             <p>Name: </p>
             <img id="productPic" src={Tester}/>
+            {/* <ReactImageMagnify {...{
+                smallImage: {
+                    alt: 'Stapler!!!!!!!!!!',
+                    isFluidWidth: true,
+                    src: Tester
+                },
+                largeImage: {
+                    src: Tester,
+                    width: 1200,
+                    height: 1800
+                }
+            }} /> */}
             <p>Price:</p>
             <p>Description: </p>
             <p>In stock: </p>
