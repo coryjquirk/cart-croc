15b0c9f7a6a7a390462793de5a12794bdb6089a4
Josh8903 <thorngren87@gmail.com>
2020-12-07 22:06:19 -0600

    redirect to home after logging in

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import { Col, Row, Container } from "../Grid";
 
 class Dashboard extends Component {
   onLogoutClick = (e) => {
@@ -13,30 +14,40 @@ class Dashboard extends Component {
     const { user } = this.props.auth;
 
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
-        <div className="row">
-          <div className="landing-copy col s12 center-align">
-            <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                Are you sure you want to log out?
-              </p>
-            </h4>
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem",
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
-            </button>
-          </div>
-        </div>
-      </div>
+      <Container fluid>
+        <h1>{user.name.split(" ")[0]}</h1>
+        <a href="/checkout">Checkout</a>
+        <br />
+        <a href="/receipts">Receipts</a>
+        <br />
+        <a href="/inventory">Inventory</a>
+        <br />
+        <button
+          style={{
+            width: "150px",
+            borderRadius: "3px",
+            letterSpacing: "1.5px",
+            marginTop: "1rem",
+          }}
+          onClick={this.onLogoutClick}
+          className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+        >
+          Logout
+        </button>
+      </Container>
+      // <div style={{ height: "75vh" }} className="container valign-wrapper">
+      //   <div className="row">
+      //     <div className="landing-copy col s12 center-align">
+      //       <h4>
+      //         <b>Hey there,</b>
+      //         <p className="flow-text grey-text text-darken-1">
+      //           Are you sure you want to log out?
+      //         </p>
+      //       </h4>
+
+      //     </div>
+      //   </div>
+      // </div>
     );
   }
 }
