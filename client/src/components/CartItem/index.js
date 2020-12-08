e7b924722f6b7bbfc5e72c85e31b65e3ec4d50b9
Eric Osowski <eric.j.osowski@icloud.com>
2020-12-08 16:51:31 -0600

    Fixing cart filter

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // https://fontawesome.com/how-to-use/on-the-web/using-with/react
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import API from "../../utils/API";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 const user = JSON.parse(localStorage.getItem('username'));
 
 
@@ -13,24 +13,22 @@ export default function CartItem() {
   const [inventoryList, setinventoryList] = useState([]);
   const [cartList, setCart] = useState([]);
   const [sellQuantity, setCartQuantity] = useState();
-  
+
   useEffect(() => {
     let inventoryItems = API.getAllItems()
     console.log(inventoryItems)
     setinventoryList(inventoryItems)
-
+    let usercart;
     const getCart = async () => {
-      let cartItems = API.getAllCartItems();
-      // We have access to user here.
-
-      var userCart = cartItems.filter(function(cartItem){
-        return cartItem.username == user;
-    });
-
-      console.log(userCart);
-      setCart(userCart);
-      }
-      getCart();
+      API.getAllCartItems()
+        .then(tempCart => {
+          var userCart = tempCart.filter(function (cartItem) {
+            return cartItem.username == user;
+          });
+          setCart(userCart);
+        })
+    }
+    getCart();
   }, [])
 
   // This code looks kinda weird, its nested in the way it is so that we can access
@@ -38,59 +36,59 @@ export default function CartItem() {
   // level functions
 
   const deleteCartItem = itemId => {
-      return event => {
-          event.preventDefault();
-          API.deleteCartItem(itemId);
-          location.reload();
-      }
+    return event => {
+      event.preventDefault();
+      API.deleteCartItem(itemId);
+      location.reload();
+    }
   };
 
   const updateCartItemSellQuantity = itemId => {
-      return event => {
-          event.preventDefault();
-          let newSellQuantity = {
-              sellQuantity: sellQuantity
-          }
-          API.updateCartItemSellQuantity(newSellQuantity, itemId);
+    return event => {
+      event.preventDefault();
+      let newSellQuantity = {
+        sellQuantity: sellQuantity
       }
+      API.updateCartItemSellQuantity(newSellQuantity, itemId);
+    }
   };
 
   function handleQuantityChange(event) {
-      const sellQuantity = event.target.value;
-      console.log("Quantity is now " + sellQuantity)
-      setCartQuantity(sellQuantity)
+    const sellQuantity = event.target.value;
+    console.log("Quantity is now " + sellQuantity)
+    setCartQuantity(sellQuantity)
   }
   return (
     <table id="cartItem">
-    
+
       {cartList?.map((result) => {
-                                return (
-                                    <tr key={result._id}>
-                                      <td>
-                                        <img
-                                          src={Tester}
-                                          alt="product preview"
-                                          id="productPreview"
-                                        ></img>
-                                      </td>
-                                      {result.itemName} | ${result.price}
-                                      <td>
-                                        <input
-                                          type="number"
-                                          id="checkoutQty"
-                                          placeholder={result.sellQuantity}
-                                        ></input>
-                                      </td>
-                                      <td>
-                                        <FontAwesomeIcon
-                                          onClick={deleteCartItem(result._id)}
-                                          icon={faTrashAlt}
-                                          id="trashIcon"
-                                        />
-                                      </td>
-                                    </tr>
-                                );
-                            })}
+        return (
+          <tr key={result._id}>
+            <td>
+              <img
+                src={Tester}
+                alt="product preview"
+                id="productPreview"
+              ></img>
+            </td>
+            {result.itemName} | ${result.price}
+            <td>
+              <input
+                type="number"
+                id="checkoutQty"
+                placeholder={result.sellQuantity}
+              ></input>
+            </td>
+            <td>
+              <FontAwesomeIcon
+                onClick={deleteCartItem(result._id)}
+                icon={faTrashAlt}
+                id="trashIcon"
+              />
+            </td>
+          </tr>
+        );
+      })}
 
     </table>
   );
