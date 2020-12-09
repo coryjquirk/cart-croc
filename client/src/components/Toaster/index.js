43121193d6147e6ccfcf0dd0fea9f9e73689a32e
Josh8903 <thorngren87@gmail.com>
2020-12-08 23:01:16 -0600

    toast added to shop page

---
+++ client/src/components/Toaster/index.js
@@ -0,0 +1,17 @@
+// import React from "react";
+
+// import { useToasts } from "react-toast-notifications";
+
+// const Toaster = ({ content }) => {
+//   const { addToast } = useToasts();
+//   return (
+// () =>
+//         addToast(content, {
+//           appearance: "success",
+//           autoDismissTimeout: 2000,
+//           autoDismiss: true,
+//         })
+//   );
+// };
+
+// export default Toaster;
